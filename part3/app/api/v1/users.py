6931cf4ccd49e055f43cb2230d356b39a770d371
AboFawaz305 <84806203+AboFawaz305@users.noreply.github.com>
2025-09-13 13:02:44 +0300

    added: Formatted the code and fixed update route dont work

--- part3/app/api/v1/users.py
+++ part3/app/api/v1/users.py
@@ -1,9 +1,9 @@
 import json
 
+from app.bcrypt import bcrypt
 from app.services import facade
 from flask_jwt_extended import get_jwt_identity, jwt_required
 from flask_restx import Namespace, Resource, fields
-from app.bcrypt import bcrypt
 
 api = Namespace("users", description="User operations")
 
@@ -25,7 +25,7 @@ user_model_update = api.model(
     {
         "first_name": fields.String(description="First name of the user"),
         "last_name": fields.String(description="Last name of the user"),
-        "email": fields.String( description="Email of the user"),
+        "email": fields.String(description="Email of the user"),
         "password": fields.String(description="password"),
     },
 )
@@ -50,7 +50,7 @@ class Users(Resource):
     def post(self):
         user = json.loads(get_jwt_identity())
         if not user["is_admin"]:
-            return {'error':"Admin privleges required"},403
+            return {"error": "Admin privleges required"}, 403
         user_json = api.payload
         if facade.get_user_by_email(user_json["email"]) is not None:
             return {"error": "Email already registered"}, 400
@@ -62,17 +62,15 @@ class Users(Resource):
             "first_name": new_user.first_name,
             "last_name": new_user.last_name,
             "email": new_user.email,
-      }, 201
-        
-    
+        }, 201
 
 
-@api.route("/<string:id>")
+@api.route("/<string:user_id>")
 class User(Resource):
     @api.response(200, "OK")
     @api.response(404, "Not found")
-    def get(self, id):
-        user = facade.get_user(id)
+    def get(self, user_id):
+        user = facade.get_user(user_id)
         if not user:
             return {"error": "Not found"}, 404
         return {
@@ -91,18 +89,20 @@ class User(Resource):
     def put(self, user_id):
         user = json.loads(get_jwt_identity())
         if not user["is_admin"]:
-            return {'error':"Admin privleges required"},403
+            return {"error": "Admin privleges required"}, 403
         user_data = api.payload
         # if not user["id"] == user_id:
         #     api.abort(400, "Bad Request")
-        if user_data["email"] is not None :
+        if "email" in user_data.keys():
             similar_emails = [
-            u for u in self.get_all_users() if u.email == user_data['email']
-        ]
+                u for u in facade.get_all_users() if u.email == user_data["email"]
+            ]
             if len(similar_emails) != 0:
-                return {'error':"email already in use"},400
-            if user_data['password'] is not None:
-                user_data['password'] = bcrypt.generate_password_hash(user_data['password']).decode('utf-8')
+                api.abort(400, "email already in use")
+        if "password" in user_data.keys():
+            user_data["password"] = bcrypt.generate_password_hash(
+                user_data["password"]
+            ).decode("utf-8")
         facade.user_repo.update(user_id, user_data)
         updated_user = facade.get_user(user_id)
         return updated_user, 200
